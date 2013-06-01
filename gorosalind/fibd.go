54c2a56746ac1b37cf304e5fbc393662e1c2838c
Szymon Sobczak <szymon.sobczak@gmail.com>
2013-06-01 17:59:37 +0200

    add readme and license notice

--- gorosalind/fibd.go
+++ gorosalind/fibd.go
@@ -1,3 +1,7 @@
+// Copyright 2013 Szymon Sobczak: http://about.me/ssobczak
+// Licensed under the MIT license: http://opensource.org/licenses/MIT
+// The above copyright notice shall be included in all copies or substantial portions of the Software.
+
 package gorosalind
 
 import (
--- main.go
+++ main.go
@@ -1,3 +1,7 @@
+// Copyright 2013 Szymon Sobczak: http://about.me/ssobczak
+// Licensed under the MIT license: http://opensource.org/licenses/MIT
+// The above copyright notice shall be included in all copies or substantial portions of the Software.
+
 package main
 
 import (
@@ -5,5 +9,5 @@ import (
 )
 
 func main() {
-	gorosalind.Lia()
+	gorosalind.Mprt()
 }
